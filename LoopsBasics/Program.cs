3163c431e9646fc47f21f8f112299fcfa140a6f3
Edon Bytyqi <edonbytyqi@outlook.com>
2025-05-29 13:25:39 +0200

    Add project files.

---
+++ LoopsBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LoopsBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            for (int counter = 10; counter >= 0; counter--) 
+            {
+                Console.WriteLine("Counter is " + counter);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
